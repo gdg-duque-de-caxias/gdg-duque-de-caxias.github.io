de259bb4b7dd136ed6bf99409f37a6d6f1a4c7f7
Lucas Trindade <lucasktrindade@gmail.com>
2017-04-05 20:42:26 -0300

    Adicionando galeria de imagens

--- src/js/script.js
+++ src/js/script.js
@@ -2,26 +2,56 @@ $(document).ready(function () {
     $("body").on("click", ".nav-toggle", function () {
         $(".nav-menu").slideToggle();
     });
+});
+function gallery () {
+  var elementLeft = document.getElementsByClassName('control__left')[0];
+  var elementRight = document.getElementsByClassName('control__right')[0];
+  var elementGallery = document.getElementsByClassName('gallery__images')[0];
+  var minX = 0;
+  var maxX = -(elementGallery.offsetWidth);
 
+  elementLeft.addEventListener('click', function (e){
+    if(this.minX >= 200) {
+      this.minX-=200;
+      console.log(this.minX);
+      //this.elementGallery.style.transform = "translateX("+this.minX+"px)"
+    } else {
+      this.minX = 0;
+      console.log(this.minX);
+      //this.elementGallery.style.transform = "translateX("+this.minX+"px)"
+    }
+  })
+
+  elementRight.addEventListener('click', function (e){
+    this.minX-= 200;
+    console.log(this.minX);
+    //this.elementGallery.style.transform = "translateX("+this.minX+"px)"
+  })
+
+}
+
+(function (){
+    gallery();
     var inputs = document.getElementsByClassName('input')
-       Array.prototype.forEach.call(inputs, function (item) {
-         item.addEventListener('focus', function () {
-           if (item.value === '') {
-             var id = item.id
-             var selector = '.label[for="'+ id + '"]'
-             var label = document.querySelector(selector)
-             item.classList.toggle('has-focus')
-             label.classList.toggle('is-active')
-           }
-         })
-         item.addEventListener('blur', function () {
-           if (item.value === '') {
-             var id = item.id
-             var selector = '.label[for="'+ id + '"]'
-             var label = document.querySelector(selector)
-             item.classList.toggle('has-focus')
-             label.classList.toggle('is-active')
-           }
-         })
-       })
-})
+    Array.prototype.forEach.call(inputs, function (item) {
+     item.addEventListener('focus', function () {
+       if (item.value === '') {
+         var id = item.id
+         var selector = '.label[for="'+ id + '"]'
+         var label = document.querySelector(selector)
+         item.classList.toggle('has-focus')
+         label.classList.toggle('is-active')
+       }
+     })
+     item.addEventListener('blur', function () {
+       if (item.value === '') {
+         var id = item.id
+         var selector = '.label[for="'+ id + '"]'
+         var label = document.querySelector(selector)
+         item.classList.toggle('has-focus')
+         label.classList.toggle('is-active')
+       }
+     })
+    })
+
+}());
